5f17a544056f7b0488e1afbf2de6d0ea257c5e87
pooi <ldayou@naver.com>
2017-09-27 17:37:42 +0900

    기능 추가   * 필요한 파일 추가   * 앱 초기 설정   * 시작 화면에 kenburnsview 적용   * 시작 화면에 로그인 폼 추가   * 시작 화면에 보호자 회원가입 버튼 추가   * (임시)로그인 버튼을 누르면 간호사 메인 메뉴로 이동하도록 함

--- app/src/main/java/cf/nearby/nearby/StartActivity.java
+++ app/src/main/java/cf/nearby/nearby/StartActivity.java
@@ -1,13 +1,68 @@
 package cf.nearby.nearby;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import cf.nearby.nearby.nurse.NurseMainActivity;
 
 public class StartActivity extends AppCompatActivity {
+
+
+    // UI
+    private KenBurnsView kenBurnsView;
+    private RelativeLayout rl_background;
+
+    private LinearLayout formLogin;
+    private MaterialEditText formId;
+    private MaterialEditText formPw;
+    private Button formLoginBtn;
+
+    private TextView tv_signupSupporterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        init();
+
+    }
+
+    private void init(){
+
+        kenBurnsView = (KenBurnsView)findViewById(R.id.image);
+        kenBurnsView.setImageResource(R.drawable.loading);
+
+        rl_background = (RelativeLayout)findViewById(R.id.rl_background);
+
+        initLoginForm();
+
+        tv_signupSupporterBtn = (TextView)findViewById(R.id.tv_signup_supporter);
+
+    }
+
+    private void initLoginForm(){
+
+        formLogin = (LinearLayout)findViewById(R.id.form_login);
+        formId = (MaterialEditText)findViewById(R.id.form_id);
+        formPw = (MaterialEditText)findViewById(R.id.form_pw);
+        formLoginBtn = (Button)findViewById(R.id.form_login_btn);
+        formLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, NurseMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/cf/nearby/nearby/nurse/NurseMainActivity.java
@@ -0,0 +1,15 @@
+package cf.nearby.nearby.nurse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cf.nearby.nearby.R;
+
+public class NurseMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_main);
+    }
+}
