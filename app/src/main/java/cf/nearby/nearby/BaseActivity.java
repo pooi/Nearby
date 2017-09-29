645c381cd6dfa8e6cae57f11b96cd16d1b79ffb0
pooi <ldayou@naver.com>
2017-09-29 10:37:06 +0900

    기능 추가   * 간호사 메인 메뉴 화면에 로그아웃 버튼 추가

--- app/src/main/java/cf/nearby/nearby/BaseActivity.java
+++ app/src/main/java/cf/nearby/nearby/BaseActivity.java
@@ -18,6 +18,15 @@ import android.view.animation.Animation;
 
 public class BaseActivity extends AppCompatActivity {
 
+    public void logout(){
+
+        Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+
+    }
+
 
     public void setFadeInAnimation(View view){
         Animation animation = new AlphaAnimation(0, 1);
--- app/src/main/java/cf/nearby/nearby/nurse/NurseMainActivity.java
+++ app/src/main/java/cf/nearby/nearby/nurse/NurseMainActivity.java
@@ -2,15 +2,34 @@ package cf.nearby.nearby.nurse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import cf.nearby.nearby.BaseActivity;
 import cf.nearby.nearby.R;
 
 public class NurseMainActivity extends BaseActivity {
 
+    private Button logoutBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nurse_main);
+
+        init();
+    }
+
+    private void init(){
+
+        logoutBtn = (Button)findViewById(R.id.btn_logout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+
     }
 }
